56130d623806a609fbb1d10ed282e14ab4de608f
Kevin Lam <lam.kevin86@gmail.com>
2018-03-07 14:55:22 -0800

    Renders gallery to proxy

--- server/server.js
+++ server/server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/images', (req, res) => {
+  db.findAll((err, results) => {
+    if (err) {
+      console.log('story err', err);
+    } else {
+      // console.log('story success', results)
+      res.json(results);
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`)
