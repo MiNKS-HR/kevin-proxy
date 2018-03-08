6d0556fb5134cce328ea01f4a93aa11b5b0966c2
Kevin Lam <lam.kevin86@gmail.com>
2018-03-07 16:20:26 -0800

    Displays reviews, photo, and similar

--- server/server.js
+++ server/server.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const request = require('request');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/images', (req, res) => {
-  db.findAll((err, results) => {
-    if (err) {
-      console.log('story err', err);
-    } else {
-      // console.log('story success', results)
-      res.json(results);
-    }
-  });
+app.use('/similar', function(req,res) {
+  var newurl = 'http://localhost:3003/similar' + req.url;
+  request(newurl).pipe(res);
+});
+
+app.use('/details', function(req,res) {
+  var newurl = 'http://localhost:3005/details' + req.url;
+  request(newurl).pipe(res);
+  console.log(newurl);
+});
+
+app.use('/experience/availableDate/', function(req,res) {
+  var newurl = 'http://localhost:3002/experience/availableDate' + req.url;
+  request(newurl).pipe(res);
+});
+
+app.use('/images/', function(req,res) {
+  var newurl = 'http://localhost:3004/images' + req.url;
+  request(newurl).pipe(res);
+});
+
+app.use('/img/', function(req,res) {
+  var newurl = 'http://localhost:3004/img' + req.url;
+  request(newurl).pipe(res);
+});
+
+app.use('/reviews/', function(req,res) {
+  var newurl = 'http://localhost:3001/reviews' + req.url;
+  request(newurl).pipe(res);
 });
 
 app.listen(port, () => {
